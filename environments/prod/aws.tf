61547c3e2dbfc8786f100ede3347a998cdd56621
caydey <caydey@protonmail.com>
2023-09-25 13:13:48 +0100

    added alias to aws provider

--- environments/prod/aws.tf
+++ environments/prod/aws.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region                   = local.region
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
+  alias = "some_provider"
 }
 
 # automatically gets aws_availability_zones as a list and is accessed in vpc.tf when setting azs value
