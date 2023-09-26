42126f48687e3c7b24aac18ba75c20f412a75887
caydey <caydey@protonmail.com>
2023-09-26 21:26:23 +0100

    fixed typo

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -6,14 +6,12 @@ terraform {
       version = "~> 4.18"
     }
   }
-  
-  terraform {
-    backend "s3" {
-      bucket         = "terraform-state-group9-container-accelerator"
-      key            = "global/s3/terraform.tfstate"
-      region         = "us-east-1"
-      dynamodb_table = "terraform-state-locking"
-    }
+
+  backend "s3" {
+    bucket         = "terraform-state-group9-container-accelerator"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
   }
 
   required_version = ">= 1.2.0"
